ab9ebb8b4f32b76ec5b00a03b51b845b75dabb29
tomatooy <ouyangzongxin@gmail.com>
2022-09-06 21:03:53 -0400

    fix

--- src/components/Departments/Departments.jsx
+++ src/components/Departments/Departments.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
 import DepartmentEle from './DepartmentEle';
 import styles from './Department.module.css'
@@ -10,12 +10,27 @@ import "swiper/css/pagination";
 // import required modules
 import { Pagination } from "swiper";
 export default function Departments() {
+    const [isMobile, setIsMobile] = useState(false)
+
+    //choose the screen size 
+    const handleResize = () => {
+        if (window.innerWidth < 600) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }
+
+    // create an event listener
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+    })
     return (
         <>
             <div className={styles.departmentBanner}>
                 <h1 className={styles.bannerH1}>OUR STRUCTURE</h1>
                 <Swiper
-                    slidesPerView={4}
+                    slidesPerView={isMobile?1:3}
                     spaceBetween={40}
                     pagination={{
                         clickable: true,
@@ -23,12 +38,12 @@ export default function Departments() {
                     modules={[Pagination]}
                     className={styles.departmentSwiper}
                 >
-                    <SwiperSlide className={styles.departmentSlide}><DepartmentEle/></SwiperSlide>
-                    <SwiperSlide className={styles.departmentSlide}><DepartmentEle/></SwiperSlide>
-                    <SwiperSlide className={styles.departmentSlide}><DepartmentEle/></SwiperSlide>
-                    <SwiperSlide><DepartmentEle/></SwiperSlide>
-                    <SwiperSlide><DepartmentEle/></SwiperSlide>
-                    <SwiperSlide><DepartmentEle/></SwiperSlide>
+                    <SwiperSlide className={styles.departmentSlide}><DepartmentEle /></SwiperSlide>
+                    <SwiperSlide className={styles.departmentSlide}><DepartmentEle /></SwiperSlide>
+                    <SwiperSlide className={styles.departmentSlide}><DepartmentEle /></SwiperSlide>
+                    <SwiperSlide><DepartmentEle /></SwiperSlide>
+                    <SwiperSlide><DepartmentEle /></SwiperSlide>
+                    <SwiperSlide><DepartmentEle /></SwiperSlide>
                 </Swiper>
             </div>
         </>
