ae9f26a874f401dd04cfde394b3ec0e767e88f7e
Zongxin <ouyangzongxin@gmail.com>
2022-09-08 00:05:13 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header'
 import {
   useRoutes
 } from "react-router-dom";
 import routes from './routes'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   const element = useRoutes(routes)
   return (
